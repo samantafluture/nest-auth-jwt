b8f09ff7f955397a90a7c80d346d99d090ccac37
Samanta Fluture <samantafluture@gmail.com>
2021-09-07 18:54:08 -0300

    create login with jwt token

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth/auth.controller';
 import { AuthService } from './auth/auth.service';
+import { JwtStrategyService } from './jwt-strategy/jwt-strategy.service';
 
 @Module({
   imports: [
@@ -13,6 +14,6 @@ import { AuthService } from './auth/auth.service';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategyService],
 })
 export class AuthModule {}
